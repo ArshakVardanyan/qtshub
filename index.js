56deb2b13b54a56cddf9e68abc668be9141c5d6f
DESKTOP-8I2UDGA\arsha <arshak89v@gmail.com>
2018-03-03 09:43:54 -0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-const IP   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var express = require('express')
+var path = require('path')
+var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var IP   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 	
 express()
   .use(express.static(path.join(__dirname, 'public')))
